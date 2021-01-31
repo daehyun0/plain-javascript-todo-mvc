a68ce3e7c0b883011f30b03c0cac5a4f9f47ebfb
DaehyunJoo <dd70296@gmail.com>
2021-02-01 04:02:24 +0900

    add button in todo-item-view

--- src/view/component/todo-item.ts
+++ src/view/component/todo-item.ts
@@ -14,9 +14,18 @@ export default class TodoItemView extends BaseView {
         todoItemHtmlElem.classList.add('p-4');
         todoItemHtmlElem.classList.add('rounded');
         todoItemHtmlElem.classList.add('shadow-sm');
+        todoItemHtmlElem.classList.add('d-flex');
+        todoItemHtmlElem.classList.add('justify-content-between');
+        todoItemHtmlElem.classList.add('align-items-center');
 
-        todoItemHtmlElem.innerHTML = `<p class="title h2 mb-0">${this.todoItem.title}</p>
-        <p class="content lead mb-0 mt-1">${this.todoItem.content}</p>`;
+        todoItemHtmlElem.innerHTML = `<div class="content">
+            <p class="title h2 mb-0">${this.todoItem.title}</p>
+            <p class="content lead mb-0 mt-1">${this.todoItem.content}</p>
+        </div>
+        <div class="button-group">
+            <button type="button" class="btn btn-danger">삭제</button>
+            <button type="button" class="btn btn-secondary">수정</button>
+        </div>`;
 
         return todoItemHtmlElem;
     }
