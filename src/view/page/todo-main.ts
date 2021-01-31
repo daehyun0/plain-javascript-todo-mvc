9b3936e08e96388e646d12039c86596395484fe9
DaehyunJoo <dd70296@gmail.com>
2021-02-01 03:25:01 +0900

    page render method implement

--- src/view/page/todo-main.ts
+++ src/view/page/todo-main.ts
@@ -17,33 +17,31 @@ export default class PageTodoMainView extends BaseView {
         const footer = document.createElement('footer');
 
         // header start
-        const todoAppTitle = document.createElement('div');
-        todoAppTitle.classList.add('jumbotron');
+        header.classList.add('p-5');
 
-        const todoAppTitleMain = document.createElement('h1');
-        todoAppTitleMain.classList.add('display-4');
-        todoAppTitleMain.innerText = 'Handmade Todo App';
-
-        const todoAppTitleSub = document.createElement('p');
-        todoAppTitleSub.classList.add('lead');
-        todoAppTitleSub.innerText = "이것은 타입스크립트로 만들어진 Todo 앱입니다.";
-
-        todoAppTitle.appendChild(todoAppTitleMain);
-        todoAppTitle.appendChild(todoAppTitleSub);
-
-        header.appendChild(todoAppTitle);
+        header.innerHTML = `<div class="jumbotron">
+            <h1 class="display-4">Handmade Todo App</h1>
+            <p class="lead">이것은 타입스크립트로 만들어진 Todo 앱입니다.</p>
+        </div>`
         // header start
 
         // main start
+        main.classList.add('pl-5');
+        main.classList.add('px-5');
+
+        main.innerHTML = `<div>asdfasd</div>`
         // main end
 
         // footer start 
+        footer.classList.add('p-5');
+        footer.innerHTML = `<p class="mb-0">Designed and built with all the love in the world by the Bootstrap team with the help of our contributors</p>
+            <p class="mb-0">Currently v0.0.1. Code licensed none.</p>`;
         // footer end
 
         pageFragment.appendChild(header);
         pageFragment.appendChild(main);
         pageFragment.appendChild(footer);
 
-        document.body.appendChild(pageFragment);
+        return pageFragment;
     }
 }
